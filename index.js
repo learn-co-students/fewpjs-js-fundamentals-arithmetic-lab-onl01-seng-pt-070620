3eb64d31757ae411d4dd672d5bed09fce2f2c07a
Cristian Deleon <libaration@gmail.com>
2021-01-09 12:16:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1*num2;
+const random = Math.floor(Math.random()*10) + 1
+const mod = 4%7
+const max = Math.max(1,2,3,4,5,6,7,8,20)
