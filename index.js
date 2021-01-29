a380f4aeccba90b048a644e8317e85261fdcb8fc
Sean <Sransonette@gmail.com>
2021-01-29 00:17:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.ceil(Math.random()*10) + 1;
+const num3 = 300
+const num4 = 296
+const mod = num3 % num4
+const max = Math.max(18, 19, 20)
